0a02dbdc0d481361be08f1e83dc6c1fa46fd3658
Germán López Pineda <me1lopig@uco.es>
2020-01-01 14:03:13 +0100

    Se añade capacidad de guardar en un archivo con control temporal

--- Empleados/fichero.cpp
+++ Empleados/fichero.cpp
@@ -4,6 +4,11 @@
 //
 
 #include <iostream>
+#include<fstream> // libreria para manejo de ficheros en C++
+# include<ctime>
+
+
+
 
 using namespace std;
 
@@ -19,29 +24,60 @@ struct datos
 int main()
 {
     // declaracion de la variable de datos
+    fstream fichero; // declaracion de la variable fichero
     datos lista[3];
     int n_datos = sizeof(lista) / sizeof(*lista); // dimenzion del array
 
+   
+// para el control temporal de la creacion de datos
+
+    time_t rawtime;
+    struct tm* timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+   
+  
+
+  
     // introduccion de los datos
 
-    for (int i = 0; i < n_datos; i++)
+    fichero.open("archivo.txt", ios::app); // crea un archivo de datos o adiciona si ya existe
+
+    if (!fichero)
     {
-        fflush(stdin); // Limpieza inicial del buffer
+        cout << "Se ha producido un error en la creacion del archivo"<<endl;
+    }
+    else
+    {
+        fichero << "\nArchivo de datos " << endl;
+        fichero<<"Fecha de adicion de los datos "<< asctime(timeinfo)<<endl;
+      
+        for (int i = 0; i < n_datos; i++)
+        {
+            fflush(stdin); // Limpieza inicial del buffer
 
-        cout << "Introduce nombre "<<i+1<<" ";
-        cin.getline(lista[i].nombre, 20, '\n');
+            cout << "Introduce nombre " << i + 1 << " ";
+            cin.getline(lista[i].nombre, 20, '\n');
+           
+            cout << "Introduce la edad ";
+            cin >> lista[i].edad;
 
-        
-        cout << "Introduce la edad " ;
-        cin >> lista[i].edad;
+            // introducimos datos en el archivo
+            fichero << "Id: " << i+1 << endl;
+            fichero << "Nombre: " << lista[i].nombre << endl;
+            fichero << "Edad: " << lista[i].edad << endl;
 
-        // limpieza del buffer
-       cin.clear();
-       cin.ignore(numeric_limits<streamsize>::max(), '\n');
-       
+            // limpieza del buffer
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        }
+
+        fichero.close(); // cierre del archivo
     }
 
 
+   
 
     cout << "Pulsa una tecla para terminar " << endl;
     cin.get();
